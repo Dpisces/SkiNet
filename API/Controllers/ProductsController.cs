7375dabc2f32e1517868c4e1e1fccb6afa508b7d
Ali <alisahab@hotmail.com>
2026-08-12 15:17:35 -0400

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProdcuts(string? brand, string? type, string? sort)
+        {
+            var spec = new ProductSpecification(brand, type,sort);
+            var products = await repo.ListAsync(spec);
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await repo.GetByIdAsync(id);
+            if (product == null) return NotFound();
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> GreateProduct(Product product)
+        {
+            repo.Add(product);
+            if (await repo.SaveAllAsync())
+            {
+                return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+            }
+
+            return BadRequest("Problem Creating product");
+
+        }
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateProduct(int id, Product product)
+        {
+            if (product.Id != id || !ProductExists(id))
+                return BadRequest("Cannot update this product");
+
+            repo.Update(product);
+            if (await repo.SaveAllAsync())
+            {
+                return NoContent();
+            }
+
+            return BadRequest("Problem updating the product");
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var product = await repo.GetByIdAsync(id);
+            if (product == null) return NotFound();
+            repo.Remove(product);
+            if (await repo.SaveAllAsync())
+            {
+                return NoContent();
+            }
+
+            return BadRequest("Problem deleting the product");
+        }
+
+        public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+        {
+            return (await repo.ListAllAsync()).Select(x=>x.Brand).Distinct().ToList();
+        }
+
+        public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+        {
+            return (await repo.ListAllAsync()).Select(x=>x.Type).Distinct().ToList();
+        }
+        
+        private bool ProductExists(int id)
+        {
+            return repo.Exists(id);
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,46 @@
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
+
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+/* if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+} */
+
+/* app.UseHttpsRedirection();
+
+app.UseAuthorization(); */
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+app.MapControllers();
+try
+{
+    app.Run();
+}
+catch (Exception ex)
+{
+    throw;
+}
+
